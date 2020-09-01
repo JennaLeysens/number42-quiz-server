e9cdbf4180fffacbb504c6d5a20527d972c7983c
JennaLeysens <jennalicowley@gmail.com>
2020-09-01 18:43:38 +0200

    Relations for answers

--- migrations/20200901161957-set-up-relations.js
+++ migrations/20200901161957-set-up-relations.js
@@ -20,10 +20,30 @@ module.exports = {
       onUpdate: "CASCADE",
       onDelete: "SET NULL",
     });
+    await queryInterface.addColumn("answers", "roundId", {
+      type: Sequelize.INTEGER,
+      references: {
+        model: "rounds",
+        key: "id",
+      },
+      onUpdate: "CASCADE",
+      onDelete: "SET NULL",
+    });
+    await queryInterface.addColumn("answers", "quizId", {
+      type: Sequelize.INTEGER,
+      references: {
+        model: "quizzes",
+        key: "id",
+      },
+      onUpdate: "CASCADE",
+      onDelete: "SET NULL",
+    });
   },
 
   down: async (queryInterface, Sequelize) => {
     await queryInterface.removeColumn("quizzes", "userId");
     await queryInterface.removeColumn("rounds", "quizId");
+    await queryInterface.removeColumn("answers", "roundId");
+    await queryInterface.removeColumn("answers", "quizId");
   },
 };
